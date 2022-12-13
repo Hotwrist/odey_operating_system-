4fb26440106f16bbdfd3b41e79aab6314d75a313
Odey John Ebinyi <odeyjhn@gmail.com>
2022-12-13 22:41:41 +0100

    Add files via upload

---
+++ Odey/src/kernel/k_lib/k_const.h
@@ -0,0 +1,10 @@
+#ifndef  __K_CONST_H
+#define  __K_CONST_H
+
+//=============================BOOT MODULES===================================
+// this sets the maximum number of boot modules to load at kernel start up!.
+#define         MAX_BOOT_MODULES            12  
+//============================================================================
+
+
+#endif
